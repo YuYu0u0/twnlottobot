8e6e1f9ca56a00124caabd2d2ccb1612d54cd588
YuYu0u0 <mnemonic3310@gmail.com>
2023-11-09 15:07:05 +0800

    建立git管理

---
+++ crawl/twnlotto.py
@@ -0,0 +1,183 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def lotto_radYM(year, month):
+    try:
+        my_headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+        # 第一次get將隨機生成的參數值取下
+        r = requests.get(
+            "https://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx", headers=my_headers)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            __VIEWSTATE = soup.find('input', id="__VIEWSTATE").get("value")
+            __VIEWSTATEGENERATOR = soup.find(
+                'input', id="__VIEWSTATEGENERATOR").get("value")
+            __EVENTVALIDATION = soup.find(
+                'input', id="__EVENTVALIDATION").get("value")
+
+            payLoad = {
+                "__EVENTTARGET": "",
+                "__EVENTARGUMENT": "",
+                "__LASTFOCUS": "",
+                "__VIEWSTATE": __VIEWSTATE,
+                "__VIEWSTATEGENERATOR": __VIEWSTATEGENERATOR,
+                "__EVENTVALIDATION": __EVENTVALIDATION,
+                "forma": "請選擇遊戲",
+                "Lotto649Control_history$chk": "radYM",
+                "Lotto649Control_history$dropYear": year,
+                "Lotto649Control_history$dropMonth": month,
+                "Lotto649Control_history$btnSubmit": "查詢",
+            }
+            # 將get的參數值跟年月份寫進payload並post請求資料
+            r = requests.post(
+                'https://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx', headers=my_headers, data=payLoad)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text, 'html.parser')
+                result = ''
+                no = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_L649_DrawTerm_\d"))
+                date = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_L649_DDate_\d"))
+                num = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_SNo\d_\d"))
+                sNum = soup.find_all("span", id=re.compile(
+                    "SuperLotto638Control_history1_dlQuery_SNo7_\d"))
+                # 將開獎號碼,日期,期別整理成字典
+                lotto_number = {}
+                for i in range(len(no)):
+                    lotto_num = []
+                    for j in range(6):
+                        lotto_num.append(num[i*6+j].text)
+
+                    lotto_number[date[i].text] = [
+                        no[i].text]+lotto_num+[sNum[i].text]
+
+            return lotto_number
+    except Exception as e:
+        print(e)
+
+
+def lotto_radNo(txtnum):
+    try:
+        my_headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+        # 第一次get將隨機生成的參數值取下
+        r = requests.get(
+            "https://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx", headers=my_headers)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            __VIEWSTATE = soup.find('input', id="__VIEWSTATE").get("value")
+            __VIEWSTATEGENERATOR = soup.find(
+                'input', id="__VIEWSTATEGENERATOR").get("value")
+            __EVENTVALIDATION = soup.find(
+                'input', id="__EVENTVALIDATION").get("value")
+
+            payLoad = {
+                "__EVENTTARGET": "",
+                "__EVENTARGUMENT": "",
+                "__LASTFOCUS": "",
+                "__VIEWSTATE": __VIEWSTATE,
+                "__VIEWSTATEGENERATOR": __VIEWSTATEGENERATOR,
+                "__EVENTVALIDATION": __EVENTVALIDATION,
+                "forma": "請選擇遊戲",
+                "Lotto649Control_history$chk": "radNO",
+                "Lotto649Control_history$txtNO": txtnum,
+                "Lotto649Control_history$btnSubmit": "查詢",
+            }
+            # 將get的參數值跟查詢期別寫進payload並post請求資料
+            r = requests.post(
+                'https://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx', headers=my_headers, data=payLoad)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text, 'html.parser')
+                result = ''
+                no = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_L649_DrawTerm_\d"))
+                date = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_L649_DDate_\d"))
+                num = soup.find_all("span", id=re.compile(
+                    "Lotto649Control_history_dlQuery_SNo\d_\d"))
+                sNum = soup.find_all("span", id=re.compile(
+                    "SuperLotto638Control_history1_dlQuery_SNo7_\d"))
+                # 將開獎號碼,日期,期別整理成字典
+                lotto_number = {}
+                for i in range(len(no)):
+                    lotto_num = []
+                    for j in range(6):
+                        lotto_num.append(num[i*6+j].text)
+
+                    lotto_number[no[i].text] = [
+                        date[i].text]+lotto_num+[sNum[i].text]
+
+            return lotto_number
+    except Exception as e:
+        print(e)
+
+
+# 將爬蟲下來的資訊從字典整理成統一格式的字串回傳
+def show_lottoNum(year, month):
+    try:
+        results = ''
+        datas = lotto_radYM(year, month)
+        if len(datas) == 0:
+            print('輸入錯誤')
+            return '輸入錯誤'
+        for data in datas:
+            result = data+" "+datas[data][0]+"期\t"
+            for i in datas[data][1:7]:
+                result += i+" "
+            result += "特別號:"+datas[data][-1]
+            results += "\n"+result
+        return results.strip()
+    except Exception as e:
+        print(e)
+
+
+# 以期別整理下來的資訊和輸入的號碼做對比以進行對獎
+def win_lotto(numbers, lotto_number, txtnum):
+    count = 0
+    if len(numbers) != 6:
+        result = '請輸入6個號碼'
+    sNum = lotto_number[txtnum][-1]
+
+    for num in numbers:
+        if num in lotto_number[txtnum]:
+            count += 1
+
+        if count == 6:
+            if sNum in num:
+                result = "恭喜中二獎"
+            else:
+                result = "恭喜中頭獎"
+        elif count == 5:
+            if sNum in num:
+                result = "恭喜中四獎"
+            else:
+                result = "恭喜中三獎"
+        elif count == 4:
+            if sNum in num:
+                result = "恭喜中六獎1000元"
+            else:
+                result = "恭喜中五獎2000元"
+        elif count == 3:
+            if sNum in num:
+                result = "恭喜中七獎400元"
+            else:
+                result = "恭喜中普獎400元"
+        else:
+            result = "沒中獎,再接再厲!!"
+
+    return result
+
+
+# 測試
+if __name__ == "__main__":
+    show_lottoNum(107, 12)
+    numbers = ['21', '07', '47', '44', '08', '13']
+    txtnum = '112000101'
+    print(lotto_radNo(txtnum))
+    print(win_lotto(numbers, lotto_radNo(txtnum), txtnum))
---
+++ main/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookHandler, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage, TextMessage, ImageSendMessage
+
+from crawl.twnlotto import lotto_radNo, show_lottoNum, win_lotto
+
+# Create your views here.
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parse = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+# 輪詢條件(全域變數)
+lotto_mode = 0
+mode2_status = False
+txtnum = ''
+
+
+@csrf_exempt
+def callback(request):
+    global lotto_mode
+    global mode2_status
+    global txtnum
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        try:
+            events = parse.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+        for event in events:
+            if isinstance(event, MessageEvent):
+                message = event.message.text
+                message_object = None
+                # 判斷是否進入對獎模式,進入輪詢
+                if (lotto_mode == 1) or (lotto_mode == 2):
+                    if message == "0":  # 離開條件
+                        lotto_mode = 0
+                        mode2_status = False
+                        message_text = "離開對獎模式"
+
+                    elif lotto_mode == 1:  # 月份對獎模式
+                        year = message.strip().split(' ')[0]
+                        month = message.strip().split(' ')[1]
+                        message_text = show_lottoNum(year, month)
+                        message_text += "\n請輸入年(民國)月份(以空格隔開)(0:離開)"
+                    elif lotto_mode == 2:
+                        if mode2_status:  # 輸入對獎號碼以空格隔開
+                            numbers = message.strip().split()
+                            message_text = win_lotto(
+                                numbers, lotto_radNo(txtnum), txtnum)
+                            message_text += "\n請輸入號碼(以空格隔開)(0:離開)"
+
+                        else:
+                            txtnum = message.strip()  # 輸入要對獎的期數
+                            message_text = "請輸入號碼(以空格隔開)(0:離開)"
+                            mode2_status = True
+
+                    message_object = TextMessage(text=message_text)
+                # 設定其他回應及進入輪詢條件
+                else:
+                    if message == "1":
+                        message_text = '月份查詢:'
+                        message_text += '\n請輸入年(民國)月份(以空格隔開):'
+                        message_object = TextMessage(text=message_text)
+                        lotto_mode = 1
+
+                    elif message == "2":
+
+                        print(type(txtnum))
+                        message_text = '期數號碼對獎模式:'
+                        message_text += '\n請輸入查詢期數:'
+                        message_object = TextMessage(text=message_text)
+                        lotto_mode = 2
+                    elif message == "你好":
+                        message_text = '你好,請按1進入月份查詢模式,按2進入期別對獎模式'
+                        message_object = TextMessage(text=message_text)
+                        print(lotto_mode)
+                    else:
+                        message_object = TextSendMessage(
+                            text="我不懂你的意思!請按1進入月份查詢模式,按2進入期別對獎模式")
+                    # if event.message.text=='hello':
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    # TextSendMessage(text='hello world')
+                    message_object,
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
